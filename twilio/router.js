4773e9a77078ec034c363ef0e8624aafcc0749d0
unknown <clayton.weller@gmail.com>
2018-09-04 21:01:57 -0600

    Testing what twilio sends

--- twilio/router.js
+++ twilio/router.js
@@ -11,16 +11,8 @@ const config = require("../config");
 const router = express.Router();
 const client = new twilio(accountSid, authToken);
 
-// client.messages
-//   .create({
-//     body: "Hello from Node",
-//     to: "+13038034589", // Text this number
-//     from: "+17206969370" // From a valid Twilio number
-//   })
-//   .then(message => console.log(message.sid));
-
 router.post("/", (req, res) => {
-  console.log(req.body);
+  console.log(req);
   res.json("GOOD");
   client.messages
     .create({
