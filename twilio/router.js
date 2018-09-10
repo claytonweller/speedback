a6d073b1efa8094b3d6b08e9bfba4e19d9deb824
unknown <clayton.weller@gmail.com>
2018-09-09 18:31:40 -0600

    twilio to .env

--- twilio/router.js
+++ twilio/router.js
@@ -1,17 +1,14 @@
 "use strict";
 
-const accountSid = "AC3b2fdd2fad71352cd60d79458c966ac0"; // Your Account SID from www.twilio.com/console
-const authToken = "467d1ad9c6cea7eb13e5be78a313c547"; // Your Auth Token from www.twilio.com/console
-
 const twilio = require("twilio");
 const express = require("express");
 const bodyParser = require("body-parser");
 const { Feedback } = require("../feedback/models");
 const { EventModel } = require("../events/models");
-const config = require("../config");
+const { TWILIO_AUTH_TOKEN, TWILIO_SID } = require("../config");
 
 const router = express.Router();
-const client = new twilio(accountSid, authToken);
+const client = new twilio(TWILIO_SID, TWILIO_AUTH_TOKEN);
 
 router.use(express.json());
 router.use(bodyParser.urlencoded({ extended: false }));
